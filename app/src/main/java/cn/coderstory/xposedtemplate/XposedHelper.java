56baba4557b1d06928c14eeddcfdcf9715a5a339
coderstory <coderstory@163.com>
2020-05-23 20:19:18 +0800

    更新一下配置

--- app/src/main/java/cn/coderstory/xposedtemplate/XposedHelper.java
+++ app/src/main/java/cn/coderstory/xposedtemplate/XposedHelper.java
@@ -1,43 +1,94 @@
 package cn.coderstory.xposedtemplate;
 
+
+import java.util.Set;
+
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 
+
+
 public class XposedHelper {
+
 
     public static void findAndHookMethod(String p1, ClassLoader lpparam, String p2, Object... parameterTypesAndCallback) {
         try {
-            XposedHelpers.findAndHookMethod(p1, lpparam, p2, parameterTypesAndCallback);
-
-        } catch (Throwable localString3) {
-            XposedBridge.log(localString3);
+            if (findClass(p1, lpparam) != null) {
+                XposedHelpers.findAndHookMethod(p1, lpparam, p2, parameterTypesAndCallback);
+            }
+        } catch (Exception e) {
+            if (BuildConfig.DEBUG)
+                XposedBridge.log(e);
         }
     }
 
-    public static void findAndHookConstructor(String p1, ClassLoader lpparam, Object... parameterTypesAndCallback) {
+    public static void hookAllConstructors(String p1, XC_MethodHook parameterTypesAndCallback) {
         try {
-            XposedHelpers.findAndHookConstructor(p1, lpparam, lpparam, parameterTypesAndCallback);
-
-        } catch (Throwable localString3) {
-            XposedBridge.log(localString3);
+            Class packageParser = findClass(p1, null);
+            hookAllConstructors(packageParser, parameterTypesAndCallback);
+        } catch (Exception e) {
+            if (BuildConfig.DEBUG)
+                XposedBridge.log(e);
         }
     }
 
-    protected static void findAndHookMethod(String p1, String p2, Object[] p3) {
+    private static Set<XC_MethodHook.Unhook> hookAllConstructors(Class<?> hookClass, XC_MethodHook callback) {
         try {
-            XposedHelpers.findAndHookMethod(Class.forName(p1), p2, p3);
-        } catch (Throwable localString3) {
-            XposedBridge.log(localString3);
+            return XposedBridge.hookAllConstructors(hookClass, callback);
+        } catch (Exception e) {
+            if (BuildConfig.DEBUG)
+                XposedBridge.log(e);
+            return null;
         }
+
     }
 
-    public static void hookAllMethods(String p1, ClassLoader lpparam, Object... parameterTypesAndCallback) {
+    public static void hookAllMethods(String p1, ClassLoader lpparam, String methodName, XC_MethodHook parameterTypesAndCallback) {
         try {
-            XposedHelpers.findAndHookConstructor(p1, lpparam, lpparam, parameterTypesAndCallback);
+            Class packageParser = findClass(p1, lpparam);
+            XposedBridge.hookAllMethods(packageParser, methodName, parameterTypesAndCallback);
+        } catch (Exception e) {
+            if (BuildConfig.DEBUG)
+                XposedBridge.log(e);
+        }
 
-        } catch (Throwable localString3) {
-            XposedBridge.log(localString3);
+    }
+
+    public void hookAllMethods(Class packageManagerServiceUtils, String verifySignatures, XC_MethodHook methodHook) {
+        try {
+            XposedBridge.hookAllMethods(packageManagerServiceUtils, verifySignatures, methodHook);
+        } catch (Exception e) {
+            if (BuildConfig.DEBUG)
+                XposedBridge.log(e);
         }
     }
+
+    @SuppressWarnings("unchecked")
+    protected static Object getDrmResultSUCCESS() {
+        Class<Enum> drmSuccess = (Class<Enum>) XposedHelpers.findClass("miui.drm.DrmManager.DrmResult", null);
+        return Enum.valueOf(drmSuccess, "DRM_SUCCESS");
+    }
+
+
+    public static Class<?> findClass(String className, ClassLoader classLoader) {
+        try {
+            return Class.forName(className,false,classLoader);
+        } catch (Exception e) {
+            if (BuildConfig.DEBUG)
+                XposedBridge.log(e);
+        }
+        return null;
+    }
+
+    public static Class<?> findClassWithOutLog(String className, ClassLoader classLoader) {
+        try {
+            return Class.forName(className,false,classLoader);
+        } catch (Exception e) {
+            // 忽略
+        }
+        return null;
+    }
+
 
 }
